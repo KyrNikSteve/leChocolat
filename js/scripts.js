9e8ce3813fdbff0c4a938c7dd897e9f914f9d64c
KyrNikSteve <kyriakos.nikolos@gmail.com>
2019-02-20 18:15:14 +0200

    Kyriakos final site

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,39 @@
-$(document).ready(function(){
-    $("#mycarousel").carousel( { interval: 2000 } );
-    $("#carousel-pause").click(function(){
+$(document).ready(function () {
+    $("#mycarousel").carousel({ interval: 2000 });
+    $("#carousel-pause").click(function () {
         $("#mycarousel").carousel('pause');
     });
-    $("#carousel-play").click(function(){
+    $("#carousel-play").click(function () {
         $("#mycarousel").carousel('cycle');
+    });
+});
+
+$(function () {
+    $('.material-card > .mc-btn-action').click(function () {
+        var card = $(this).parent('.material-card');
+        var icon = $(this).children('i');
+        icon.addClass('fa-spin-fast');
+
+        if (card.hasClass('mc-active')) {
+            card.removeClass('mc-active');
+
+            window.setTimeout(function () {
+                icon
+                    .removeClass('fa-arrow-left')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-bars');
+
+            }, 800);
+        } else {
+            card.addClass('mc-active');
+
+            window.setTimeout(function () {
+                icon
+                    .removeClass('fa-bars')
+                    .removeClass('fa-spin-fast')
+                    .addClass('fa-arrow-left');
+
+            }, 800);
+        }
     });
 });
